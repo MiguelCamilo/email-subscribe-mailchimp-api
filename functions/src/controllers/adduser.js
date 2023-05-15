1487f38e48a0104880ae5ac1d9c547a51cad6e37
Miguel Camilo <miguelcamilo48@gmail.com>
2023-05-15 15:12:02 -0400

    API completed building frontend

--- functions/src/controllers/adduser.js
+++ functions/src/controllers/adduser.js
@@ -11,9 +11,9 @@ export const export_account = async (req, res) => {
   const { firstname, lastname, email } = req.body;
   const list_id = "76f5877f0b";
 
-  if(!firstname || !lastname || !email) {
-    return res.status(400).send({ message: "missing required fields" })
-  }
+  // if(!firstname || !lastname || !email) {
+  //   return res.status(400).send({ message: "missing required fields" })
+  // }
 
 	try {
 		const response = await client.lists.addListMember(list_id, {
